51b9ab83411277eca627b8c3eed087373cd24ab4
HyeonJu <khju0203@hanmail.net>
2017-05-23 13:50:13 +0900

    git sample

--- app/src/main/java/com/bussystemforblind/bsb/stt_test.java
+++ app/src/main/java/com/bussystemforblind/bsb/stt_test.java
@@ -4,22 +4,29 @@ import android.content.Intent;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
 
-public class stt_test extends ActionBarActivity {
+public class stt_test extends ActionBarActivity implements OnInitListener {
 
     Intent i;
     SpeechRecognizer mRecognizer;
-    TextView textView3;
+    EditText stopname_input;
+    private TextToSpeech myTTS;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stt_test);
+        myTTS = new TextToSpeech(this,this);
 
         i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         i.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
@@ -29,7 +36,7 @@ public class stt_test extends ActionBarActivity {
         mRecognizer.setRecognitionListener(listener);
         mRecognizer.startListening(i);
 
-        textView3 = (TextView) findViewById(R.id.textView3);
+        stopname_input = (EditText) findViewById(R.id.stopname_input);
     }
 
 
@@ -71,7 +78,7 @@ public class stt_test extends ActionBarActivity {
             ArrayList<String> mResult = results.getStringArrayList(key);
             String[] rs = new String[mResult.size()];
             mResult.toArray(rs);
-            textView3.setText("" + rs[0]);
+            stopname_input.setText("" + rs[0]);
             mRecognizer.startListening(i);
         }
 
@@ -85,4 +92,14 @@ public class stt_test extends ActionBarActivity {
 
         }
     };
+
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+       String text = "정류장이름을 말씀해주세요.";
+        myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
 }
